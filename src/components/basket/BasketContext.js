59fc0c4abc0e7e8349864ded1d2a98518e9bd004
meta-lite <nick@myceliumnetworks.com>
2024-06-10 14:25:15 -0500

    fix:

--- src/components/basket/BasketContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const BasketContext = createContext({
-  assetInfoList: [],
-  getTPS: () => {},
-  searchAssets: () => {}
-});
-
-export default BasketContext;
