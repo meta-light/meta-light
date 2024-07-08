6a0aa9b7762745b482b28c1dd23bde13b6b5749f
meta-lite <nick@myceliumnetworks.com>
2024-07-08 10:44:35 -0500

    lfg

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,10 +6,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-            <Script src="/capital/scripts/jquery.min.js" strategy="beforeInteractive" />
-          	<Script src="/capital/scripts/browser.min.js" strategy="beforeInteractive" />
-          	<Script src="/capital/scripts/breakpoints.min.js" strategy="beforeInteractive" />
-          	<Script src="/capital/scripts/util.js" strategy="beforeInteractive"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=optional" rel="stylesheet" />
+          <Script src="/capital/scripts/jquery.min.js" strategy="beforeInteractive" />
+          <Script src="/capital/scripts/browser.min.js" strategy="beforeInteractive" />
+          <Script src="/capital/scripts/breakpoints.min.js" strategy="beforeInteractive" />
+          <Script src="/capital/scripts/util.js" strategy="beforeInteractive"/>
         </Head>
         <body>
           <Main />
