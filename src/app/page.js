efaca2c0e586f780ffd3274e97a0944ae6edfdec
meta-lite <nick@myceliumnetworks.com>
2024-08-28 19:44:46 -0500

    LFG

--- src/app/page.js
+++ src/app/page.js
@@ -167,7 +167,7 @@ const Home = () => {
                   <h3>Get in Touch</h3>
                   <div className="contactTypes">
                     <div className="flexContainer"><div className="typeBox"><i className="lni lni-envelope"></i><a href="mailto:nick@carpinito.id">nick@carpinito.id</a><p>Email Me</p></div></div>
-                    <div className="flexContainer"><div className="typeBox"><i className="lni lni-calendar"></i><a href="https://calendly.com/nick-carpinito/30min" target="_blank" rel="noopener noreferrer">Calendly</a><p>Schedule a Meeting</p></div></div>
+                    <div className="flexContainer"><div className="typeBox"><i className="lni lni-calendar"></i><a href="https://carp.youcanbook.me/" target="_blank" rel="noopener noreferrer">Calendar</a><p>Schedule a Meeting</p></div></div>
                   </div>
                 </div> 
               </section>
